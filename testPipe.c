92908f82c5cad3b52cf5d662befc592147787dab
mdiannna <marusicdiana@gmail.com>
2018-01-10 10:33:34 +0200

    "code refactor and some history/pipes bugs solved"

--- testPipe.c
+++ testPipe.c
@@ -1,4 +1,4 @@
-// The  following  program  creates  a pipe, and then fork(2)s to create a
+j// The  following  program  creates  a pipe, and then fork(2)s to create a
 //   child process; the child inherits a duplicate set of  file  descriptors
 //   that  refer  to  the same pipe.  After the fork(2), each process closes
 //   the descriptors that it doesn't need for the pipe (see  pipe(7)).   The
